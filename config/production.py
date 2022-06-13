99d8ec63944cc73c7e251732967fd8c9893c5316
홍수연 <hongsooyeon87@gmail.com>
2022-06-13 14:19:30 +0900

    내용수정

--- config/production.py
+++ config/production.py
@@ -2,8 +2,8 @@ from config.default import *
 import os
 from dotenv import load_dotenv
 import MySQLdb
-import pymysql
-pymysql.install_as_MYSQLdb()
+# import pymysql
+# pymysql.install_as_MYSQLdb()
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x1a\xdf3\x07\x8e\x9br/\x90\xb6B\x91I!C\xfa'
