1ea1f025c964c2462ca9391722c7fb91555419e8
홍수연 <hongsooyeon87@gmail.com>
2022-06-15 12:46:15 +0900

    별적립 수정

--- pybo/views/mypage_views.py
+++ pybo/views/mypage_views.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, render_template, flash, request
+from flask import Blueprint, url_for, render_template, flash, request, g
 from werkzeug.utils import redirect
 
 from pybo import db
 from pybo.forms import UserLoginForm
-from pybo.models import Question, Answer, User
+from pybo.models import Question, Answer, User, Photo
 from .auth_views import login_required
+from sqlalchemy import func
 
 bp = Blueprint('mypage', __name__, url_prefix='/mypage')
 
 @bp.route('/modify/', methods=('GET', 'POST'))
 def modify_info():
-    return render_template('mypage/modify_info.html')
+    current_user = g.user.id
+    star = db.session.query(Photo).filter(Photo.user_id.like(current_user)).count()
+    return render_template('mypage/modify_info.html', variable=star)
+
